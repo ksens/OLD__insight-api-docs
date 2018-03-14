417aa0f540a6dc8d09f4c0b59e5110e24ffeef5d
Kriti Sen Sharma <ksen@paradigm4.com>
2018-03-13 22:00:05 -0400

    Update the book

--- _deploy.sh
+++ _deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-set -e
+set -x
 
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "ksen@paradigm4.com"
+git config --global user.name "Kriti Sen Sharma"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m "Update the book" || true
 git push -q origin gh-pages
